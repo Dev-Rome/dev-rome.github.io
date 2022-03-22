4b744191aa656ca077f3e1102b2d7c9d38b91441
dev-rome <84602714+dev-rome@users.noreply.github.com>
2022-03-22 02:38:53 -0400

    Updated app.js

--- js/app.js
+++ js/app.js
@@ -1,219 +1,150 @@
 "use strict";
 
-// dom elements
-const categoryTitle = document.querySelector(".category__title");
-const categoryList = document.querySelector(".category__list");
-const score = document.querySelector(".score");
-const timer = document.querySelector(".time");
-const question = document.querySelector(".question");
-const questionNumber = document.querySelector(".question__number");
-const modal = document.querySelector(".modal__window");
-const overlay = document.querySelector(".overlay");
+// initailize variables
+const score = document.querySelector(".game__score-number");
+const modal = document.querySelector(".modal__container");
+const modalTitle = document.querySelector(".modal__title");
+const modalText = document.querySelector(".modal__text");
+const startButton = document.querySelector(".start__btn");
+const resetButton = document.querySelector(".reset__btn");
+const questionNumber = document.querySelector(".question__number-number");
+const questionText = document.querySelector(".question");
+const questionTimer = document.querySelector(".question__timer-number");
+const lives = document.querySelector(".lives__container");
 
-// using Array.from to convert nodelist to array
 const answerText = Array.from(document.getElementsByClassName("answer__text"));
 
-// dom elements for categorie buttons
-const genralKnowledge = document.getElementById("general__button");
-const sports = document.getElementById("sports__button");
-const entertainment = document.getElementById("entertainment__button");
-const science = document.getElementById("science__button");
-const history = document.getElementById("history__button");
-const reset = document.getElementById("reset__button");
-
-// game variables
-let numberOfQuestions = 10;
+let numberOfQuestions = 20;
 let questions = [];
 let allQuestions = [];
 let currentQuestion = {};
+let scoreGoal = 100;
 let questionCounter = 0;
 let acceptingAnswers = false;
 let pauseTimer = false;
 let playerScore, timeValue;
 
-// function to call data from API
-function triviaAPI(id) {
-  const url = `https://opentdb.com/api.php?amount=50&category=${id}&type=multiple`;
-  fetch(url)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      questions = data.results.map((question) => {
-        const triviaQuestion = {
-          question: question.question,
-        };
-        //   using spread operator to create array of incorrect answers
-        const answerArray = [question.correct_answer, ...question.incorrect_answers];
-        // using mathfloor with mathrandom to get random number between 0 and 3 adding 1 to get number between 1 and 4
-        triviaQuestion.answer = Math.floor(Math.random() * 4) + 1;
-        // answerArray.forEach is used to loop through the array and set the answer text to the answer array
-        answerArray.forEach((answer, index) => {
-          triviaQuestion["answer" + (index + 1)] = answer;
-        });
-        return triviaQuestion;
+const url = "https://opentdb.com/api.php?amount=50&category=15&type=multiple";
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    questions = data.results.map((questions) => {
+      const triviaQuestion = {
+        question: questions.question,
+      };
+      const answerArray = [...questions.incorrect_answers];
+      triviaQuestion.answer = Math.floor(Math.random() * 4) + 1;
+      answerArray.splice(
+        triviaQuestion.answer - 1,
+        0,
+        questions.correct_answer
+      );
+      answerArray.forEach((answer, index) => {
+        triviaQuestion["answer" + (index + 1)] = answer;
       });
-      startGame();
-    })
-    .catch((err) => {
-      console.log(err, "Something went wrong");
+      return triviaQuestion;
     });
-}
+    startGame();
+  })
+  .catch((error) => console.log(error));
 
-// foreach to loop throught answer and add correct class
-answerText.forEach((answer) => {
-  // add event listener to each answer
-  answer.addEventListener("click", (e) => {
-    // if accepting answers is true
-    // and if the answer clicked is the correct answer
-    // add right class to answer
-    // add to player score
-    // if wrong answer, add wrong class to answer
-    if (!acceptingAnswers) return;
-    acceptingAnswers = false;
-    const userAnswer = e.target.dataset["number"];
-    const correctOrIncorrect =
-      userAnswer == currentQuestion.answer ? "right" : "wrong";
-    if (correctOrIncorrect === "right") {
-      playerScore += 10;
-      score.innerHTML = playerScore;
-    }
-    e.target.parentElement.classList.add(correctOrIncorrect);
-    setTimeout(() => {
-      e.target.parentElement.classList.remove(correctOrIncorrect);
-      getNewQuestion();
-    }, 1000);
-  });
-});
-
-// general knowledge api
-function genralKnowledgeAPI() {
-    categoryTitle.innerHTML = "General Knowledge";
-    categoryList.classList.add("hide");
-    setTimeout(() => {
-        categoryList.classList.add("hide");
-        triviaAPI(9);
-    }, 2000);
-}
-
-// sports api
-function sportsApi() {
-    categoryTitle.innerHTML = "Sports";
-    categoryList.classList.add("hide");
-    setTimeout(() => {
-        categoryList.classList.add("hide");
-        triviaAPI(21);
-    }, 2000);
-}
-
-// entertainment api
-function entertainmentAPI() {
-    categoryTitle.innerHTML = "Entertainment";
-    categoryList.classList.add("hide");
-    setTimeout(() => {
-        categoryList.classList.add("hide");
-        triviaAPI(11);
-    }, 2000);
-}
-
-// science api
-function scienceAPI() {
-    categoryTitle.innerHTML = "Science";
-    categoryList.classList.add("hide");
-    setTimeout(() => {
-        categoryList.classList.add("hide");
-        triviaAPI(17);
-    }, 2000);
-}
-
-// history api
-function historyAPI() {
-    categoryTitle.innerHTML = "History";
-    categoryList.classList.add("hide");
-    setTimeout(() => {
-        categoryList.classList.add("hide");
-        triviaAPI(23);
-    }, 2000);
-}
-
-// function to start game
 function startGame() {
-  questionCounter = 0;
-  playerScore = 0;
-  timeValue = 45;
+  if (modal.classList.contains("hide")) {
+    pauseTimer = false;
+  } else {
+    pauseTimer = true;
+  }
   allQuestions = [...questions];
-  modal.classList.add("hide");
-  overlay.classList.add("hide");
-  pauseTimer = false;
-  gameTimer();
+  playerScore = 0;
+  questionCounter = 1;
+  timeValue = 45;
   getNewQuestion();
+  checkAnswer();
+  gameTimer();
 }
 
-// function to get new question
 function getNewQuestion() {
-  // if there are no more questions, end game
-  // if questionCounter is greater than equal to number of questions, end game
   if (allQuestions.length === 0 || questionCounter >= numberOfQuestions) {
     endGame();
   }
-  questionCounter++;
-  questionNumber.innerHTML = `${questionCounter} / ${numberOfQuestions}`;
-  // get random question from all questions array and set current question to that question
+  
+  questionNumber.innerHTML = questionCounter;
   const currentQuestionIndex = Math.floor(Math.random() * allQuestions.length);
   currentQuestion = allQuestions[currentQuestionIndex];
-  question.innerHTML = currentQuestion.question;
-  // loop through answer letters and set text to answer text
-  answerText.forEach((answer) => {
-    const number = answer.dataset["number"];
-    answer.innerHTML = currentQuestion["answer" + number];
+  questionText.innerHTML = currentQuestion.question;
+  answerText.forEach((answer, index) => {
+    answer.innerHTML = currentQuestion["answer" + (index + 1)];
   });
-  // remove question from array
   allQuestions.splice(currentQuestionIndex, 1);
   acceptingAnswers = true;
+  questionCounter++;
 }
 
-// function game timer
+function checkAnswer() {
+  answerText.forEach((answer) => {
+    answer.addEventListener("click", (e) => {
+      if (!acceptingAnswers) {
+        return;
+      }
+      acceptingAnswers = false;
+      const selectedAnswer = e.target;
+      const selectedAnswerIndex = selectedAnswer.dataset["number"];
+      const classToApply =
+        selectedAnswerIndex == currentQuestion.answer ? "correct" : "incorrect";
+      if (classToApply === "correct") {
+        playerScore += 5;
+        score.innerHTML = playerScore;
+        answer.style.backgroundColor = "correct";
+        answer.innerHTML = "Correct!";
+      } else {
+        answer.innerHTML = "Incorrect!";
+        displayBrokenHeart();
+      }
+      selectedAnswer.parentElement.classList.add(classToApply);
+      setTimeout(() => {
+        selectedAnswer.parentElement.classList.remove(classToApply);
+        getNewQuestion();
+      }, 1000);
+    });
+  });
+}
+
+function displayBrokenHeart() {
+    const heart = document.createElement("i");
+    heart.classList.add("fa", "fa-heart-broken", "broken");
+    lives.appendChild(heart);
+    if (lives.children.length === 3) {
+        setTimeout(endGame, 700);
+    }
+}
+
 function gameTimer() {
   if (pauseTimer) return;
   if (timeValue <= 0) {
     endGame();
   } else if (timeValue > 0) {
     timeValue--;
-    timer.innerHTML = timeValue;
+    questionTimer.innerHTML = timeValue;
     setTimeout(gameTimer, 1000);
   }
 }
 
-// function to end game
-// or if user wants to pick a new category
 function endGame() {
-  acceptingAnswers = false;
-  pauseTimer = true;
-  modal.classList.remove("hide");
-  overlay.classList.remove("hide");
-  categoryTitle.innerHTML = `Game Over - Your Score is ${playerScore} out of 100 points`;
-  categoryList.classList.add("hide");
-  reset.classList.remove("hide");
+    pauseTimer = true;
+    modal.classList.remove("hide");
+    modalTitle.innerHTML = "Game Over";
+    modalText.innerHTML = `Your final score is ${playerScore} out of ${scoreGoal}`;
+    startButton.classList.add("hide");
+    resetButton.classList.remove("hide");
 }
 
-function resetGame() {
-  playerScore = 0;
-  score.innerHTML = playerScore;
-  questionCounter = 0;
-  timeValue = 45;
-  timer.innerHTML = timeValue;
-  reset.classList.add("hide");
-  categoryList.classList.remove("hide");
-  categoryTitle.innerHTML = "Select a Category";
-  allQuestions = [...questions];
-  pauseTimer = true;
-  getNewQuestion();
-}
+startButton.addEventListener("click", () => {
+  modal.classList.add("hide");
+  startGame();
+});
 
-// button event listeners to select category
-genralKnowledge.addEventListener("click", genralKnowledgeAPI);
-sports.addEventListener("click", sportsApi);
-entertainment.addEventListener("click", entertainmentAPI);
-science.addEventListener("click", scienceAPI);
-history.addEventListener("click", historyAPI);
-reset.addEventListener("click", resetGame);
+resetButton.addEventListener("click", () => {
+    modal.classList.add("hide");
+    lives.innerHTML = "";
+    startGame();
+});
